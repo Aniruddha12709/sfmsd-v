ed86d7a558e7372a333097562c62aab5623a88d4
Aniruddha12709 <85880752+Aniruddha12709@users.noreply.github.com>
2021-08-04 15:48:23 +0530

    Add files via upload

---
+++ Project_Template_24-main/Project_Template_24-main/js/PlayerArrow.js
@@ -0,0 +1,11 @@
+class PlayerArrow {
+    shoot(archerAngle) {
+        velocity = p5.Vector.fromAngle(archerAngle);
+        velocity.mult(20);
+        Matter.Body.setStatic(this.body, false);
+        Matter.Body.setVelocity(this.body, {x: velocity.x, y: velocity.y });
+
+        
+        
+    }
+}
